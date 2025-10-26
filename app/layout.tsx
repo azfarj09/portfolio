42855fde5b34faf68740e96db29bdf16425c9ce6
Azfar Jamil <arghargh408@gmail.com>
2025-10-26 11:54:10 -0400

    updated icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,11 +42,10 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: '16x16', type: 'image/x-icon' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon.svg', type: 'image/svg+xml' }
     ],
-    shortcut: '/favicon.ico',
+    shortcut: '/favicon.png',
     apple: '/apple-touch-icon.png',
   },
   manifest: '/site.webmanifest',
